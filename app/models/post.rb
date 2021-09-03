6bb0b5126c3a82c821c6b60ad331be6abf61ab94
mun1029 <ring01061995@gmail.com>
2021-09-04 03:34:24 +0900

    postモデル設定

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,18 +1,18 @@
 class Post < ApplicationRecord
-belongs_to :user
-has_many :favorites
-has_many :favorite_users, through: :favorites, source: :user
-belongs_to_active_hash :l_cate
-belongs_to_active_hash :m_cate
-belongs_to_active_hash :s_cate
+  belongs_to :user
+  has_many :favorites
+  has_many :favorite_users, through: :favorites, source: :user
+  belongs_to_active_hash :l_cate
+  belongs_to_active_hash :m_cate
+  belongs_to_active_hash :s_cate
 
-with_options presence: true do
-  validates :title
-  validates :text
-  #  セキリュティ上のことを考えcateにもpresenceを適用させてます、不要と思ったら消してもらっても大丈夫です。
-  validates :l_cate_id
-  validates :m_cate_id
-  validates :s_cate_id
-  # //
-end
+  with_options presence: true do
+    validates :title
+    validates :text
+    #  セキリュティ上のことを考えcateにもpresenceを適用させてます、不要と思ったら消してもらっても大丈夫です。
+    validates :l_cate_id
+    validates :m_cate_id
+    validates :s_cate_id
+    # //
+  end
 end
