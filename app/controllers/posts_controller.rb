95ce12a6b4c4001e73a5d45ed86794c357cc79ff
mun1029 <ring01061995@gmail.com>
2021-09-09 17:51:49 +0900

    postの投稿機能とカテゴリーの実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,10 +5,28 @@ class PostsController < ApplicationController
   end
 
   def new
-    
+    @post = Post.new
+    @category_parent_array = ["---"]
+    Category.where(ancestry: nil).each do |parent|
+      @category_parent_array << parent.name
+    end
+  end
+
+  def get_category_children
+    @category_children = Category.find_by(name: "#{params[:parent_name]}", ancestry: nil).children
+  end
+
+  def get_category_grandchildren
+    @category_grandchildren = Category.find_by(name: "#{params[:child_name]}").children
   end
 
   def create
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_to root_path
+    else
+      render :index
+    end
   end
 
   def edit
@@ -19,4 +37,12 @@ class PostsController < ApplicationController
     @post.destroy
     redirect_to user_path(current_user.id)
   end
+
+  private
+
+  def post_params
+    category = Category.find_by(name: params[:category_id])
+    params.permit(:title, :text).merge(user_id: current_user.id, category_id: category.id )
+  end
+
 end
