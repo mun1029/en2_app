f5f18841ca0085b7c66994b7a14c4589bd3d7c0d
wakachico <86215584+wakachico@users.noreply.github.com>
2021-09-12 22:27:22 +0900

    ポストモデル単体テスト

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,5 +1,37 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @post = FactoryBot.build(:post)
+  end
+
+  describe '投稿の保存' do
+    context '投稿が保存できる場合' do
+      it 'Postsテーブルカラムが全て存在すればが保存できる' do
+        expect(@post).to be_valid
+      end
+    end
+    context '投稿が保存でない場合' do
+      it 'titleが空では投稿が保存できない' do
+        @post.title = ""
+        @post.valid?
+        expect(@post.errors.full_messages).to include("Title can't be blank")
+      end
+      it 'textが空では投稿が保存できない' do
+        @post.text = ""
+        @post.valid?
+        expect(@post.errors.full_messages).to include("Text can't be blank")
+      end
+      it 'category_idが存在しないと投稿が保存できない' do
+        @post.category_id = nil
+        @post.valid?
+        expect(@post.errors.full_messages).to include("Category must exist")
+      end
+      it 'ユーザーが紐付いていなければ投稿が保存できない' do
+        @post.user = nil
+        @post.valid?
+        expect(@post.errors.full_messages).to include("User must exist")
+      end
+    end
+  end
 end
